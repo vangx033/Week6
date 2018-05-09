4ed20fa06f819075fcc456ef4060fa953e284664
vangx033 <35471018+vangx033@users.noreply.github.com>
2018-05-09 02:06:39 -0500

    Update Week6.sh

--- Week6.sh
+++ Week6.sh
@@ -2,5 +2,17 @@ Chapter 6 script
 
 # Viewing the system's runlevel
 who -r
+sleep 3
+
+# View a list of active units 
+systemctl list-units
+sleep 3
+
+# View current jobs on the system
+systemctl list-jobs
+
+# View what's happening on the system
+initctl list
+
 
 
